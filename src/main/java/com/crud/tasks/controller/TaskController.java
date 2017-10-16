a37418d79ce1fe54c0ffd5d57a599731d972cd10
“MichalPolinkiewicz <michal.polinkiewicz@gmail.com>
2017-10-16 09:19:54 +0200

    First attempt to Heroku

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -3,15 +3,12 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
-import org.aspectj.lang.annotation.RequiredTypes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
-import static javax.jnlp.DownloadService2.APPLICATION;
-import static javax.swing.text.html.HTML.Tag.FORM;
 import static org.springframework.http.MediaType.*;
 
 /**
@@ -38,11 +35,11 @@ public class TaskController {
     }
 
     @RequestMapping (method = RequestMethod.PUT, value = "updateTask", consumes = APPLICATION_FORM_URLENCODED_VALUE)
-    public TaskDto updateTask(TaskDto taskDto){
+    public TaskDto updateTask(@RequestBody TaskDto taskDto){
         return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
     }
 
-    @RequestMapping (method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
+    @RequestMapping (method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_FORM_URLENCODED_VALUE)
     public void createTask (@RequestBody TaskDto taskDto){
         service.saveTask(taskMapper.mapToTask(taskDto));
     }
