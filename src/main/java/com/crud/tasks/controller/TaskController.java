ab5f97a785ca43eeb3177a798b282a353164dc50
“MichalPolinkiewicz <michal.polinkiewicz@gmail.com>
2017-10-16 09:39:20 +0200

    JAR and PostgreSQL fixed

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -35,12 +35,12 @@ public class TaskController {
     }
 
     @RequestMapping (method = RequestMethod.PUT, value = "updateTask", consumes = APPLICATION_JSON_VALUE)
-    public TaskDto updateTask(TaskDto taskDto){
+    public TaskDto updateTask(@RequestBody TaskDto taskDto){
         return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
     }
 
     @RequestMapping (method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
-    public void createTask (TaskDto taskDto){
+    public void createTask (@RequestBody TaskDto taskDto){
         service.saveTask(taskMapper.mapToTask(taskDto));
     }
 
