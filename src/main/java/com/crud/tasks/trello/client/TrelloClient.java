a23a8d6f96f903191312bfda8503aa82b504288d
“MichalPolinkiewicz <michal.polinkiewicz@gmail.com>
2017-11-03 00:03:08 +0100

    heroku

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -38,11 +38,10 @@ public class TrelloClient {
                 .queryParam("fields", "name,id")
                 .queryParam("lists", "all").build().encode().toUri();
 
-        String adress = "https://api.trello.com/1//members/mpoltest/boards?key=cd66b6be349795ce74bcffc843d7411b&token=d5188" +
-                "341b36688e33a7d9682f7386e64d26c55c64c0b3221d823360f7338c206&fields=name&id&lists=all";
+        String adress = "https://api.trello.com/1//members/mpoltest/boards?key=cd66b6be349795ce74bcffc843d7411b&token=d5188341b36688e33a7d9682f7386e64d26c55c64c0b3221d823360f7338c206&fields=name&id&lists=all";
 
         try {
-            TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+            TrelloBoardDto[] boardsResponse = restTemplate.getForObject(adress, TrelloBoardDto[].class);
             return Arrays.asList(Optional.ofNullable(boardsResponse).orElse(new TrelloBoardDto[0]));
         } catch (RestClientException e){
             LOGGER.error (e.getMessage(), e);
